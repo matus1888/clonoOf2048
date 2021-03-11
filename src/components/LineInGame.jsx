3f58ee7f0a9f449b9d1caeae5c5e179ab3d2b020
matus1888 <matus1888@gmail.com>
2021-03-11 10:16:46 +0300

    add preventDefault() for keyDown

--- src/components/LineInGame.jsx
+++ src/components/LineInGame.jsx
@@ -33,18 +33,24 @@ class LineInGame extends React.Component {
     keyFunction(event) {
         if (!this.state.pressingLock) {
             if (event.keyCode === 40) {
+                event.preventDefault()
                 this.setState({...this.state, pressingLock: true})
                 this.props.down()
             } else if (event.keyCode === 38) {
+                event.preventDefault()
                 this.setState({...this.state, pressingLock: true})
                 this.props.up()
             } else if (event.keyCode === 37) {
+                event.preventDefault()
                 this.setState({...this.state, pressingLock: true})
-                console.log(Date.now()+'props.left()')
+                // console.log(Date.now()+'props.left()')
                 this.props.left()
             } else if (event.keyCode === 39) {
+                event.preventDefault()
                 this.setState({...this.state, pressingLock: true})
                 this.props.right()
+            }else{
+                return
             }
 
         }
