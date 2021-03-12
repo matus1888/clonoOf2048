567655f718f275d36de48467082e2be00347e9b6
Alex <39013396+matus1888@users.noreply.github.com>
2021-03-12 17:44:45 +0300

    Update Cell.jsx

--- src/components/gameField/lineInField/cell/Cell.jsx
+++ src/components/gameField/lineInField/cell/Cell.jsx
@@ -1,76 +1,31 @@
 import React from "react";
-import s from "../../../LineInGame.module.css";
-import f from "./Cell.module.css"
+import lineInGameStyles from "../../../LineInGame.module.css";
+import styleOfCell from "./Cell.module.css"
 import {getAnimationEffect} from "../GetAnimationEffect";
 
 
 class Cell extends React.Component {
+
+
     render() {
         let getColor = (val) => {
-            let x = Math.log2(val)
-            if (val === 0) {
-                return f.zero
-            } else {
-                switch (x) {
-                    case 1: {
-                        return f.one
-                    }
-                    case 2: {
-                        return f.two
-                    }
-                    case 3: {
-                        return f.three
-                    }
-                    case 4: {
-                        return f.four
-                    }
-                    case 5: {
-                        return f.five
-                    }
-                    case 6: {
-                        return f.six
-                    }
-                    case 7: {
-                        return f.seven
-                    }
-                    case 8: {
-                        return f.eight
-                    }
-                    case 9: {
-                        return f.nine
-                    }
-                    case 10: {
-                        return f.ten
-                    }
-                    case 11: {
-                        return f.eleven
-                    }
-                    case 12: {
-                        return f.twelve
-                    }
-                    case 13: {
-                        return f.thirteen
-                    }
-                    case 14: {
-                        return f.fourteen
-                    }
-                    case 15: {
-                        return f.fifteen
-                    }
-                    case 16: {
-                        return f.sixteen
-                    }
-                    default : {
-                        return f.sixteen
-                    }
-                }
-            }
+            const cellStyles = [styleOfCell.zero,
+                styleOfCell.one, styleOfCell.two,
+                styleOfCell.three, styleOfCell.four,
+                styleOfCell.five, styleOfCell.six,
+                styleOfCell.seven, styleOfCell.eight,
+                styleOfCell.nine, styleOfCell.ten,
+                styleOfCell.eleven, styleOfCell.twelve,
+                styleOfCell.thirteen, styleOfCell.fourteen,
+                styleOfCell.fifteen, styleOfCell.sixteen];
+            console.log('value= ' + val + ' getColor return a:' + cellStyles[Math.log2(val)])
+            return cellStyles[Math.log2(val)];
         }
-        let an=getAnimationEffect(this.props.animation)
+        let an = getAnimationEffect(this.props.animation)
         return (
-            <div className={`${s.cellStyle} ${getColor(this.props.value)}
-            ${an.changed&&an.effect} `}>
-                <p className={s.p}>{this.props.value}</p></div>
+            <div className={`${lineInGameStyles.cellStyle} ${getColor(this.props.value)}
+            ${an.changed && an.effect} `}>
+                <p className={lineInGameStyles.p}>{(this.props.value) ? this.props.value : null}</p></div>
         )
     }
 }
